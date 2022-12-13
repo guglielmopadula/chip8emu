93168d3224e3d278bbdb4e0da93fcd8530e616b7
Niccolo Tosato <niccolo.tosato@yahoo.it>
2022-12-13 20:05:59 +0100

    removed clear_register and added ram clear register test

--- src/test/java/org/dssc/chip8/Tests.java
+++ src/test/java/org/dssc/chip8/Tests.java
@@ -13,8 +13,15 @@ public class Tests {
         Registers reg=new Registers();
         Byte[] temp=new Byte[16];
         Arrays.fill(temp,(byte) 0);
-        reg.clear();
         assertTrue(Arrays.equals(reg.V,temp));
+    }
+    @Test
+    void test_clear_ram(){
+        RAM ram=new RAM();
+        Byte[] temp=new Byte[4096];
+        Arrays.fill(temp,(byte) 0);
+        ram.clear();
+        assertTrue(Arrays.equals(ram.memory,temp));
     }
 
 
