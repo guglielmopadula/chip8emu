adaa243183f85e5af7cd7877b103bcc7f20a9d7a
Niccolo Tosato <niccolo.tosato@yahoo.it>
2023-01-11 22:30:10 +0100

    add screen test, removed clear ram test

--- src/test/java/org/dssc/chip8/Tests.java
+++ src/test/java/org/dssc/chip8/Tests.java
@@ -9,9 +9,27 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import static java.awt.image.BufferedImage.TYPE_BYTE_BINARY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
  class Tests {
+     static boolean compareImages(BufferedImage Image1, BufferedImage Image2){
+         int width=(int) Image1.getWidth();
+         int height=(int) Image2.getHeight();
+         boolean flag=true;
+         for(int i=0; i<width; i++){
+             for (int j=0; j<height; j++){
+                 flag=flag & (Image1.getRGB(i,j)==Image2.getRGB(i,j));
+             }
+         }
+
+         return flag;
+     }
 
     @Test
     void testClearRegister(){
@@ -20,13 +38,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
         Arrays.fill(temp,(int) 0);
         assertTrue(Arrays.equals(reg.v,temp));
     }
-    @Test
-    void testClearRam() {
-        RAM ram=new RAM();
-        Integer[] temp=new Integer[4096];
-        Arrays.fill(temp,(int) 0);
-        assertTrue(Arrays.equals(ram.memory,temp));
-    }
+
+    //@Test
+    //void testClearRam() {
+    //    RAM ram=new RAM();
+    //    Integer[] temp=new Integer[4096];
+    //    Arrays.fill(temp,(int) 0);
+    //    assertTrue(Arrays.equals(ram.memory,temp));
+    //}
 
     @ParameterizedTest
     @ValueSource(strings = {"rom1", "rom2"})
@@ -77,9 +96,49 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
             assertTrue(false,"IO exception");
             throw new IOException(e);
         }
-        System.out.println("ciao");
 
     }
 
+     @Test
+     void test_compare_images_true() {
+         BufferedImage Image1 = new BufferedImage(10, 20,TYPE_BYTE_BINARY);
+         Image1.setRGB(0, 0, Color.WHITE.getRGB());
+         BufferedImage Image2 = new BufferedImage(10, 20, TYPE_BYTE_BINARY);
+         Image2.setRGB(0, 0, Color.WHITE.getRGB());
+         assertEquals(true, compareImages(Image1, Image2));
+     }
 
-}
+     @Test
+     void test_compare_images_false() {
+         BufferedImage Image1 = new BufferedImage(20, 10,TYPE_BYTE_BINARY);
+         Image1.setRGB(0, 0, Color.WHITE.getRGB());
+         BufferedImage Image2 = new BufferedImage(20, 10, TYPE_BYTE_BINARY);
+         Image2.setRGB(0, 0, Color.BLACK.getRGB());
+         assertEquals(false,compareImages(Image1, Image2));
+     }
+     @Test
+     void test_render_pixel(){
+         Screen screen1= new Screen(1);
+         screen1.DrawPixel(0,1);
+         assertEquals(true, Color.WHITE.getRGB()==screen1.getPixel(0,1));
+     }
+
+
+
+     @Test
+     void test_snapshot() {
+         Screen screen1 = new Screen(1);
+         screen1.DrawPixel(0, 0);
+         screen1.DrawPixel(1, 0);
+         System.out.println(screen1.getPixel(0,0));
+         BufferedImage Image1= screen1.snapshot();
+         BufferedImage Image2 = new BufferedImage(64, 32,TYPE_BYTE_BINARY);
+         Image2.setRGB(0, 0, Color.WHITE.getRGB());
+         Image2.setRGB(1, 0, Color.WHITE.getRGB());
+         assertEquals(true,compareImages(Image1, Image2));
+     }
+
+
+
+
+ }
