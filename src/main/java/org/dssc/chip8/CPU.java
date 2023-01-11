e75eef6515c5b33a076505e7e0cc6e938437d855
Niccolo Tosato <niccolo.tosato@yahoo.it>
2023-01-11 22:29:30 +0100

    add Screen

--- src/main/java/org/dssc/chip8/CPU.java
+++ src/main/java/org/dssc/chip8/CPU.java
@@ -9,6 +9,7 @@ class CPU {
     Stack stack;
     Timers timers;
     short  pc;
+    //private Boolean[][]  chip8_pixels = new Boolean[64][32]; //
 
 
     CPU(Keyboard keyboard,RAM ram, Registers registers, Screen screen, Timers timers){
@@ -27,5 +28,38 @@ class CPU {
         highByte = ram.memory[this.pc+1];
          opcode=((lowByte<<8) | highByte);
         return opcode;
+        //NON VOGLIO MERGIARE
     }
+
+    void decodeExecute(int opcode) {
+        switch (opcode & 0xf000) {
+            case 0x8000:
+                //this.registers.v[]
+                //this.registers.v[]
+                break;
+            case 0XD000:
+                //this OPCODE will render a sprite
+                int N = opcode & 0x000f;
+                int Vx = this.registers.v[(opcode & 0x0f00) >> 8];
+                int Vy = this.registers.v[(opcode & 0x00f0) >> 4];
+                this.renderSprite(Vx,Vy,N);
+                break;
+        }
+    }
+    void renderSprite(int x, int y, int N){
+        //this.chip8_pixels[][];
+        //this.screen.render(chip8_pixels);
+        for(int riga=0;riga<N;riga++){
+            for (int colonna=0;colonna<8;colonna++){
+                screen.DrawPixel(riga,colonna);
+            }
+        }
+        //opzione 1 - this.screen.drawpixel()
+        //opzione 2 - scrivo solo su chip8_pixel poi faccio una routine che renderizza a 30 FPS
+        //opzione 3 - a ogni draw scrivo tutto quanto
+
+    }
+
+
+
 }
--- src/main/java/org/dssc/chip8/Screen.java
+++ src/main/java/org/dssc/chip8/Screen.java
@@ -1,5 +1,45 @@
 package org.dssc.chip8;
+import java.awt.Color;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static java.awt.image.BufferedImage.TYPE_BYTE_BINARY;
 
 public class Screen {
+    final int WHITE  = Color.WHITE.getRGB();
+    final int BLACK  = Color.BLACK.getRGB();
+    private static int height = 32;
+    private static int width = 64;
+    private static int scale=5;
+    private BufferedImage backend;
+    //private Boolean[][]  chip8_pixels = new Boolean[width][height];
+    public Screen(int scale) {
+        this.scale=scale;
+        backend=new BufferedImage(width*scale,height*scale,TYPE_BYTE_BINARY);
+        int[] temp= new int[32*64*scale*scale];
+        Arrays.fill(temp,Color.BLACK.getRGB());
+        backend.setRGB(0,0,64*scale,32*scale,temp,0, 64*scale);
+    }
+
+    private void clear_screen(){
+        int[] temp= new int[32*64*this.scale*this.scale];
+        Arrays.fill(temp,Color.BLACK.getRGB());
+        backend.setRGB(0,0,64*scale,32*scale,temp,0, 64*scale);
+    }
+    public void DrawPixel(int i, int j){
+        int[] temp=new int[scale*scale];
+        Arrays.fill(temp,Color.WHITE.getRGB());
+        backend.setRGB(i*scale,j*scale,scale,scale,temp,0, scale);
+    }
+
+    public int getPixel(int i, int j){
+        return backend.getRGB(i*scale,j*scale);
+    }
+
+    public BufferedImage snapshot(){
+        return backend;
+    }
 
 }
